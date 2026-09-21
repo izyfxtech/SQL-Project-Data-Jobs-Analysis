a28233d1186e5428a54b554ee99e89f5192e8c3a
izyfxtech <ieberendu.official@gmail.com>
2026-09-21 23:30:34 +0100

    Update file paths in SQL load script to Windows format

--- sql_load/3_modify_tables.sql
+++ sql_load/3_modify_tables.sql
@@ -25,10 +25,10 @@ NOTE: If you are having issues with permissions. And you get error:
 
 -- NOTE: This has been updated from the video to fix issues with encoding
 
-\COPY company_dim FROM '/home/izy/Documents/SQL/csv_files/company_dim.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+\COPY company_dim FROM 'C:\Users\ieber\OneDrive\Documents\SQL-PROJECT-DATA-JOBS-ANALYSIS\csv_files\company_dim.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
 
-\COPY skills_dim FROM '/home/izy/Documents/SQL/csv_files/skills_dim.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+\COPY skills_dim FROM 'C:\Users\ieber\OneDrive\Documents\SQL-PROJECT-DATA-JOBS-ANALYSIS\csv_files\skills_dim.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
 
-\COPY job_postings_fact FROM '/home/izy/Documents/SQL/csv_files/job_postings_fact.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+\COPY job_postings_fact FROM 'C:\Users\ieber\OneDrive\Documents\SQL-PROJECT-DATA-JOBS-ANALYSIS\csv_files\job_postings_fact.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
 
-\COPY skills_job_dim FROM '/home/izy/Documents/SQL/csv_files/skills_job_dim.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+\COPY skills_job_dim FROM 'C:\Users\ieber\OneDrive\Documents\SQL-PROJECT-DATA-JOBS-ANALYSIS\csv_files\skills_job_dim.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
