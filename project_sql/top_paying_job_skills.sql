9b6fb0c72a3e0188e4c67078c9cec5b63e8314cc
izyfxtech <ieberendu.official@gmail.com>
2026-09-23 13:21:19 +0100

    Update SQL scripts to correct file paths for data loading

--- project_sql/top_paying_job_skills.sql
+++ project_sql/top_paying_job_skills.sql
@@ -6,17 +6,50 @@ Question: What skills are required for the top-paying data analyst jobs?
     helping job seekers understand which skills to develop that align with top salaries
 */
 
-SELECT
-    job_id,
-    job_title,
-    company_dim.name AS company_name,
-    salary_year_avg
-FROM
-    job_postings_fact
-LEFT JOIN
-    company_dim
-ON
-    job_postings_fact.company_id = company_dim.company_id
+/*
+Solution:
+CTE of the top 10 highest paying jobs
+JOINED with the skills_job_dim table and then the skills_dim table
+Formatted the skills onto one row via STRING_AGG()
+*/
+WITH top_paying_jobs AS(
+    SELECT
+        job_id,
+        job_title,
+        company_dim.name AS company_name,
+        salary_year_avg
+    FROM
+        job_postings_fact
+    LEFT JOIN
+        company_dim
+    ON
+        job_postings_fact.company_id = company_dim.company_id
 WHERE
-    job_location ILIKE "Anywhere" AND
-    job
+    job_location ILIKE 'Anywhere' AND
+    job_title = 'Data Analyst' AND
+    salary_year_avg IS NOT NULL
+ORDER BY
+    salary_year_avg DESC
+LIMIT 10
+)
+SELECT
+    top_paying_jobs.job_id,
+    top_paying_jobs.company_name,
+    top_paying_jobs.salary_year_avg,
+    STRING_AGG(skills_dim.skills, ', ') AS skills
+FROM
+    top_paying_jobs
+JOIN 
+    skills_job_dim
+ON 
+    top_paying_jobs.job_id = skills_job_dim.job_id
+JOIN 
+    skills_dim    
+ON 
+    skills_job_dim.skill_id = skills_dim.skill_id
+GROUP BY
+    top_paying_jobs.job_id,
+    top_paying_jobs.company_name,
+    top_paying_jobs.salary_year_avg
+ORDER BY
+    salary_year_avg DESC;
